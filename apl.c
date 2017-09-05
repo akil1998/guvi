f52d798bf0cee2f5e9c915383c8a0cc4871fae7b
akilsiva <30828660+akil1998@users.noreply.github.com>
2017-09-05 22:13:46 +0530

    Create apl.c

---
+++ apl.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+printf("enter the alphabet/n");
+scanf("%d",&a);
+if(a>=b && b<=z)
+{
+printf("the given word is alphabet");
+}
+else
+{
+printf("the given word is not alphabet");
+}
+}
