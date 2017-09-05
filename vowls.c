31391cf8f6a983cc264687ad65c3f6912aaf5ed0
akilsiva <30828660+akil1998@users.noreply.github.com>
2017-09-05 15:59:25 +0530

    Create vowls.c

---
+++ vowls.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char n;
+printf("enter the letters");
+scanf("%d",&n);
+if(n==a||n==e||n==i||n==o||n==u)
+{
+printf("the given value is vowel");
+}
+else
+{
+printf("the given value is not vowel");
+}
+}
