80bc9383fe56c61cfd50eb211d811fa4e8e0a982
akilsiva <30828660+akil1998@users.noreply.github.com>
2017-09-05 15:51:19 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the number /n");
+scanf("%d",&a);
+if(a%2==0)
+{
+the given number is even");
+}
+else
+{
+the given number is positive");
+}
+}
