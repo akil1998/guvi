d628d4128791a65e4e47ba2d186b265266f3912c
akilsiva <30828660+akil1998@users.noreply.github.com>
2017-09-05 15:46:56 +0530

    Create posi.c

---
+++ posi.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the number /n");
+scanf("%d",&a);
+if(a>0)
+{
+printf("the given value is positive");
+}
+if(a<0)
+{
+printf("the given number is negative");
+}
+else
+{
+printf("the given number is zero");
+}
+}
