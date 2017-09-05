2d3088188d17bf51620142f72f0c9e12b46a912c
akilsiva <30828660+akil1998@users.noreply.github.com>
2017-09-05 22:34:39 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+printf("enter the value of  a/n");
+scanf("%d",&a);
+printf("enter the value of b /n");
+scanf("%d",&b);
+c=a+b;
+printf(" the sum of a and b is");
+}
