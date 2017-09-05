e3162cc31420e6aa23d40aa30b767f66394c1ba9
akilsiva <30828660+akil1998@users.noreply.github.com>
2017-09-05 23:14:06 +0530

    Update maxi.c

--- maxi.c
+++ maxi.c
@@ -3,20 +3,21 @@
 #include<math.h>
 void main()
 {
-   long int a[20],i,j,n,temp,c;
-   long int sum=0;
-   clrscr();
-   scanf("%ld",&n);
+long int a[20],i,j,n,temp,c;
+long int sum=0;
+clrscr();
+   printf("enter the values/n");
+scanf("%ld",&n);
 for(i=0;i<n;i++)
 {
-  scanf("%d",&a[i]);
+scanf("%d",&a[i]);
 }
 for(i=0;i<n;i++)
 {
- for(j=i+1;j<n;j++)
- {
-  if(a[j]>a[i])
- {
+for(j=i+1;j<n;j++)
+{
+if(a[j]>a[i])
+{
   temp=a[i];
   a[i]=a[j];
   a[j]=temp;
