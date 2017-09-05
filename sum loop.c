5943f6bd9cb81329e4823a36ec924eab06a62257
akilsiva <30828660+akil1998@users.noreply.github.com>
2017-09-05 22:41:15 +0530

    Update sum loop.c

--- sum loop.c
+++ sum loop.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
-int main()
+void main()
 {
-    int n, i, sum = 0;
-    
-    printf("Enter a positive integer: ");
-    scanf("%d",&n);
-
-    for(i=1; i <= n; ++i)
-    {
-        sum += i;   // sum = sum+i;
-    }
-
-    printf("Sum = %d",sum);
-
-    return 0;
+int n, i, sum = 0;
+printf("Enter a positive integer: ");
+scanf("%d",&n);
+for(i=1; i <= n; ++i)
+{
+sum += i;   // sum = sum+i;
+}
+printf("Sum = %d",sum);
+return 0;
 }
