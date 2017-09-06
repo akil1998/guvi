b3d229b0becd55c5847d6db686726dbb099503f6
akilsiva <30828660+akil1998@users.noreply.github.com>
2017-09-06 21:54:19 +0530

    Create holy.c

---
+++ holy.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char a;
+printf("enter the day:");
+scanf("%d",&a);
+if(a==sunday)
+{
+printf("the given day is holy day");
+}
+else
+{
+printf("the given day is working day");
+}
+}
