ad2725d3666790d7dd5981a0dd877843d553ad1e
akilsiva <30828660+akil1998@users.noreply.github.com>
2017-09-06 20:57:31 +0530

    Create mul.c

---
+++ mul.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("enter the value of a:");
+scanf("%d",@a);
+printf("enter the value of b:");
+scanf("%d",&b);
+c=a*b;
+printf("muliplication of %d and %d is:");
+scanf("%d",&c);
+}
