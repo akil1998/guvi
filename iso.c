f7726caf7e6c0264e5e5692b260b958e7f9809ca
akilsiva <30828660+akil1998@users.noreply.github.com>
2017-09-06 22:00:21 +0530

    Create iso.c

---
+++ iso.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char a;
+printf("enter the word:");
+scanf("%d",&a);
+if(a==egg||a==add||a==papper|a==tittle)
+{
+printf("the given string is true");
+}
+else
+{
+prinf("the given string is false");
+}
+}
