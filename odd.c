7ab6074b04868fae74eaaad25b9efaa7be1ab05b
akilsiva <30828660+akil1998@users.noreply.github.com>
2017-09-12 16:55:20 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,i;
+printf("enter the value from starting number");
+scanf("%d",&a);
+printf("enter the the last number");
+scanf("%d",&b);
+for(i=a;i<=b;i++)
+{
+c=a%3;
+}
+return 0;
+printf("the value is :");
+scanf("%d",&c);
+}
+}
