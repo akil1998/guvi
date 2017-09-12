de3cf6dd6dff3056d9ef6e00d7678ac9d43d85cc
akilsiva <30828660+akil1998@users.noreply.github.com>
2017-09-12 15:13:13 +0530

    Create reversee.c

---
+++ reversee.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d;
+printf("enter the value of a");
+scanf("%d"&a);
+printf("enter the value of b");
+scanf("%d"&b);
+printf("enter the value of c");
+scanf("%d"&c);
+printf("enter the value of d");
+scanf("%d"&d);
+printf("%d%d%d");
+}
