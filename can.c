4c7c8871f82a3853c2ecca05d79a4f36712af837
akilsiva <30828660+akil1998@users.noreply.github.com>
2017-09-14 11:21:11 +0530

    Create can.c

---
+++ can.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+3include<conio.h>
+void main()
+{
+void cmp(const void *a,const void *b){
+    return (*(int*)a)-(*(int*)b);
+}
+int main()
+{
+    int n,i,sum=0;
+    scanf("%d",&n);
+    sum=n;
+    int hash[n];
+    int rating[n];
+    for(i=0;i<n;i++){
+        hash[i]=0;
+    }
+    for(i=0;i<n;i++){
+        scanf("%d",&rating[i]);
+    }
+    for(i=1;i<n-1;i++){
+        if(rating[i-1]<rating[i]){
+            hash[i]=hash[i-1]+1;
+        }
+    }
+    for(i=n-2;i>=0;i--){
+        if(rating[i]>rating[i+1]){
+            if(hash[i]>hash[i+1]){
+                hash[i]=hash[i]+1;
+            }
+            else{
+                hash[i]=hash[i+1]+1;
+            }
+        }
+    }
+    for(i=1;i<n;i++){
+        
+        sum=sum+hash[i];
+    }
+    printf("%d",sum);
+    return 0;
+}
